124fca0c00c1654d5a6eb193e9fc98eefbcd604f
AgnesYe1029 <71740907+ziyuan1029@users.noreply.github.com>
2021-04-09 18:05:37 +0800

    profile documentation
    BindingUtils done, HealthInfoHistoryAdapter done.

--- app/src/main/java/com/example/tryonlysports/ui/profile/BindingUtils.kt
+++ app/src/main/java/com/example/tryonlysports/ui/profile/BindingUtils.kt
@@ -3,61 +3,124 @@ package com.example.tryonlysports.ui.profile
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
+<<<<<<< Updated upstream
+=======
+/**
+ * This is the util function to bind the WorkoutHistory date to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
+>>>>>>> Stashed changes
 @BindingAdapter("dateWorkoutHistoryText")
 fun TextView.setDateWorkoutHistoryText(item: WorkoutHistory) {
     text = item.date?.toDate().toString()
 }
 
+/**
+ * This is the util function to bind the WorkoutHistory workout type to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
 @BindingAdapter("typeWorkoutHistoryText")
 fun TextView.setTypeWorkoutHistoryText(item: WorkoutHistory) {
     text = item.workoutType.toString()
 }
 
+/**
+ * This is the util function to bind the WorkoutHistory average speed to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
 @BindingAdapter("speedWorkoutHistoryText")
 fun TextView.setSpeedWorkoutHistoryText(item: WorkoutHistory) {
     text = item.avgSpeed.toString()
 }
 
+/**
+ * This is the util function to bind the WorkoutHistory duration to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
 @BindingAdapter("durationWorkoutHistoryText")
 fun TextView.setDurationWorkoutHistoryText(item: WorkoutHistory) {
     text = item.duration.toString()
 }
 
+/**
+ * This is the util function to bind the WorkoutHistory total calories to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
 @BindingAdapter("caloriesWorkoutHistoryText")
 fun TextView.setCaloriesWorkHistoryText(item: WorkoutHistory) {
     text = item.calories.toString()
 }
 
+/**
+ * This is the util function to bind the WorkoutHistory total distance to the display text.
+ *
+ * @param item the WorkoutHistory object to be bound into a text field.
+ */
 @BindingAdapter("distanceWorkoutHistoryText")
 fun TextView.setDistanceWorkoutHistoryText(item: WorkoutHistory) {
     text = item.distance.toString()
 }
 
+/**
+ * This is the util function to bind the HealthInfoHistory record time to the display text.
+ *
+ * @param item the HealthInfoHistory object to be bound into a text field.
+ */
 @BindingAdapter("dateWeightHistoryListText")
 fun TextView.setDateWeightHistoryListText(item: HealthInfoHistory) {
     text = item.recordTime?.toDate().toString()
 }
 
+/**
+ * This is the util function to bind the HealthInfoHistory weight history to the display text.
+ *
+ * @param item the HealthInfoHistory object to be bound into a text field.
+ */
 @BindingAdapter("weightHistoryListText")
 fun TextView.setWeightHistoryListText(item: HealthInfoHistory) {
     text = item.value.toString()
 }
 
+/**
+ * This is the util function to bind the ScheduleHistory description to the display text.
+ *
+ * @param item the ScheduleHistory object to be bound into a text field.
+ */
 @BindingAdapter("description")
 fun TextView.setDescription(item: ScheduleHistory) {
     text = item.description
 }
 
+/**
+ * This is the util function to bind the ScheduleHistory location to the display text.
+ *
+ * @param item the ScheduleHistory object to be bound into a text field.
+ */
 @BindingAdapter("location")
 fun TextView.setLocation(item: ScheduleHistory) {
     text = item.location
 }
 
+/**
+ * This is the util function to bind the ScheduleHistory startDateTime to the display text.
+ *
+ * @param item the ScheduleHistory object to be bound into a text field.
+ */
 @BindingAdapter("starttime")
 fun TextView.setStartTime(item: ScheduleHistory) {
     text = item.startDateTime?.toDate().toString()
 }
 
+/**
+ * This is the util function to bind the ScheduleHistory endDateTime to the display text.
+ *
+ * @param item the ScheduleHistory object to be bound into a text field.
+ */
 @BindingAdapter("endtime")
 fun TextView.setEndTime(item: ScheduleHistory) {
     text = item.endDateTime?.toDate().toString()
--- app/src/main/java/com/example/tryonlysports/ui/profile/HealthInfoHistoryAdapter.kt
+++ app/src/main/java/com/example/tryonlysports/ui/profile/HealthInfoHistoryAdapter.kt
@@ -10,28 +10,63 @@ import com.example.tryonlysports.databinding.ListWeightHistoryBinding
 <<<<<<< Updated upstream
 =======
 /**
- * This is the custom adapter class for displaying the health info histories.
+ * This is the custom adapter class for displaying the health info history.
  *
  * @author Liu Zhixuan, Li Rui, Ye Ziyuan
  */
 >>>>>>> Stashed changes
 class HealthInfoHistoryAdapter: ListAdapter<HealthInfoHistory, HealthInfoHistoryAdapter.ViewHolder>(HealthInfoHistoryDiffCallback()) {
+<<<<<<< Updated upstream
 
+=======
+    /**
+     * Creates a new ViewHolder object whenever the RecyclerView needs a new one
+     *
+     * @param parent the ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType the type of the new View
+     * @return a new ViewHolder that holds a View of the given view type
+     */
+>>>>>>> Stashed changes
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HealthInfoHistoryAdapter.ViewHolder {
         return ViewHolder.from(parent)
     }
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * Displays the data at the specified position
+     *
+     * @param holder a ViewHolder that describes an item view and metadata about its place within the RecyclerView.
+     * @param position the specific position that the data will be displayed.
+     */
+>>>>>>> Stashed changes
     override fun onBindViewHolder(holder: HealthInfoHistoryAdapter.ViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
     }
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * This is the ViewHolder class that describes an item view and metadata about its place within the RecyclerView.
+     *
+     * @property binding the databinding of the ListWeightHistory
+     */
+>>>>>>> Stashed changes
     class ViewHolder private constructor(val binding: ListWeightHistoryBinding): RecyclerView.ViewHolder(binding.root) {
+        /**
+         * Binds the HealthInfoHistory object to the recycler item.
+         *
+         * @param item the HealthInfoHistory data object.
+         */
         fun bind(item: HealthInfoHistory) {
             binding.healthInfo = item
             binding.executePendingBindings()
         }
 
+        /**
+         * The companion object that inflates the view with ViewHolder.
+         */
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
@@ -44,13 +79,38 @@ class HealthInfoHistoryAdapter: ListAdapter<HealthInfoHistory, HealthInfoHistory
 
 }
 
+<<<<<<< Updated upstream
 class  HealthInfoHistoryDiffCallback : DiffUtil.ItemCallback<HealthInfoHistory>() {
 
+=======
+/**
+ * This is the callback class to facilicate the HealthInfoHistory content recycler display by checking the difference.
+ *
+ */
+class  HealthInfoHistoryDiffCallback : DiffUtil.ItemCallback<HealthInfoHistory>() {
+    /**
+     * Checks if the item IDs are the same.
+     *
+     * @param oldItem the previous info bound to the recycler view.
+     * @param newItem the new info to bind to the recycler view.
+     * @return boolean value (true if the id of the oldItem is the same as the id of the newItem, false otherwise)
+     */
+>>>>>>> Stashed changes
     override fun areItemsTheSame(oldItem: HealthInfoHistory, newItem: HealthInfoHistory): Boolean {
         return oldItem.id == newItem.id
     }
 
+<<<<<<< Updated upstream
 
+=======
+    /**
+     * Checks if the contents of the items are the same.
+     *
+     * @param oldItem the previous info bound to the recycler view.
+     * @param newItem the new info to bind to the recycler view.
+     * @return boolean value (true if the oldItem has the same content as the newItem, false otherwise)
+     */
+>>>>>>> Stashed changes
     override fun areContentsTheSame(oldItem: HealthInfoHistory, newItem: HealthInfoHistory): Boolean {
         return oldItem == newItem
     }
