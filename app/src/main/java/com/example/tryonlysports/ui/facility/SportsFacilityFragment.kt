53a0b5232f8ceda8199a35e0c3d70fe789489092
AgnesYe1029 <71740907+ziyuan1029@users.noreply.github.com>
2021-04-12 09:56:26 +0800

    Rewrite the facility documentation
    Rewrite

--- app/src/main/java/com/example/tryonlysports/ui/facility/SportsFacilityFragment.kt
+++ app/src/main/java/com/example/tryonlysports/ui/facility/SportsFacilityFragment.kt
@@ -41,6 +41,7 @@ import com.google.maps.android.data.kml.KmlLayer
 
 import org.json.JSONObject
 
+<<<<<<< Updated upstream
 
 class SportsFacilityFragment : Fragment(), GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener,
 GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
@@ -49,6 +50,39 @@ GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
     var mMapView: MapView? = null
     private var googleMap: GoogleMap? = null
 
+=======
+/**
+ * This is the Fragment for the displaying sports facility on a google map
+ *
+ * @author Li Rui, Ye Ziyuan, Wan Qian
+ */
+class SportsFacilityFragment : Fragment(), GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener,
+GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
+
+    /**
+     * The SportsFacilityViewModel.
+     */
+    private lateinit var sportsFacilityViewModel: SportsFacilityViewModel
+
+    /**
+     * The MapView to display GoogleMap on sports facility fragment.
+     */
+    var mMapView: MapView? = null
+
+    /**
+     * The GoogleMap object to display the sports facility map.
+     */
+    private var googleMap: GoogleMap? = null
+
+    /**
+     * Creates fragment portion of the view hierarchy that initiates the SportsFacilityViewModel
+     *
+     * @param inflater converts xml file fragment_sportsfacility into viewobjects
+     * @param container a view that contains other views
+     * @param savedInstanceState a reference to a Bundle object that is passed into the onCreate method of MainActivity.
+     * @return a view to display on the sports facility map page
+     */
+>>>>>>> Stashed changes
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -90,15 +124,17 @@ GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
             googleMap?.setMinZoomPreference(10.0f)
             googleMap?.setMaxZoomPreference(13.0f)
 
+<<<<<<< Updated upstream
             // For dropping a marker at a point on the Map
+=======
+>>>>>>> Stashed changes
             val sg = LatLng(1.3521, 103.8198)
-            // For dropping a marker at a point on the Map
 
-//            googleMap?.addMarker(
-//                MarkerOptions().position(sg).title("Marker Title").snippet("Marker Description")
-//            )
 
+<<<<<<< Updated upstream
             // For zooming automatically to the location of the marker
+=======
+>>>>>>> Stashed changes
             val cameraPosition = CameraPosition.Builder().target(sg).zoom(12f).build()
             googleMap?.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
             addLayer()
@@ -107,6 +143,13 @@ GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
         return rootView
     }
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * Adds a GeoJson layer onto the sports facility map.
+     *
+     */
+>>>>>>> Stashed changes
     fun addLayer() {
         super.onStart()
         val geoJsonLineLayer = GeoJsonLayer(googleMap, R.raw.sgsportfacilities, context )
@@ -120,98 +163,43 @@ GoogleMap.OnInfoWindowLongClickListener, GoogleMap.OnInfoWindowCloseListener{
 
     }
 
-    inner class MyItem(
-        lat: Double,
-        lng: Double,
-        title: String,
-        snippet: String
-    ) : ClusterItem {
-
-        private val position: LatLng
-        private val title: String
-        private val snippet: String
-
-        override fun getPosition(): LatLng {
-            return position
-        }
-
-        override fun getTitle(): String? {
-            return title
-        }
-
-        override fun getSnippet(): String? {
-            return snippet
-        }
-
-        init {
-            position = LatLng(lat, lng)
-            this.title = title
-            this.snippet = snippet
-        }
-    }
-
-
+    /**
+     * Performs actions once the function is clicked.
+     *
+     * @param p0 the marker object that has been clicked.
+     * @return boolean indicating whether the marker is clicked.
+     */
     override fun onMarkerClick(p0: Marker?): Boolean {
         TODO("Not yet implemented")
     }
 
+    /**
+     * Performs actions once the info window is clicked.
+     *
+     * @param p0 the marker object which generates the info window.
+     */
     override fun onInfoWindowClick(p0: Marker?) {
         TODO("Not yet implemented")
     }
 
+    /**
+     * Performs actions once the info window is long clicked.
+     *
+     * @param p0 the marker object which generates the info window.
+     */
     override fun onInfoWindowLongClick(p0: Marker?) {
         TODO("Not yet implemented")
     }
 
+    /**
+     * Performs actions once the info window is closed.
+     *
+     * @param p0 the marker object which generates the info window.
+     */
     override fun onInfoWindowClose(p0: Marker?) {
         TODO("Not yet implemented")
     }
 
-//    internal inner class CustomInfoWindowAdapter : GoogleMap.InfoWindowAdapter {
-//        private val window: View = layoutInflater.inflate(R.layout.custom_info_window, null)
-//        // These are both view groups containing an ImageView with id "badge" and two
-//        // TextViews with id "title" and "snippet".
-//        private val contents: View = layoutInflater.inflate(R.layout.custom_info_contents, null)
-//
-//        override fun getInfoWindow(marker: Marker): View? {
-//            render(marker, window)
-//            return window
-//        }
-//
-//        override fun getInfoContents(marker: Marker): View? {
-//
-//            render(marker, contents)
-//            return contents
-//        }
-//
-//        private fun render(marker: Marker, view: View) {
-//
-//            // Set the title and snippet for the custom info window
-//            val title: String? = marker.title
-//            val titleUi = view.findViewById<TextView>(R.id.title)
-//
-//            if (title != null) {
-//                // Spannable string allows us to edit the formatting of the text.
-//                titleUi.text = SpannableString(title).apply {
-//                    setSpan(ForegroundColorSpan(RED), 0, length, 0)
-//                }
-//            } else {
-//                titleUi.text = ""
-//            }
-//
-//            val snippet: String? = marker.snippet
-//            val snippetUi = view.findViewById<TextView>(R.id.snippet)
-//            if (snippet != null && snippet.length > 12) {
-//                snippetUi.text = SpannableString(snippet).apply {
-//                    setSpan(ForegroundColorSpan(MAGENTA), 0, 10, 0)
-//                    setSpan(ForegroundColorSpan(BLUE), 12, snippet.length, 0)
-//                }
-//            } else {
-//                snippetUi.text = ""
-//            }
-//        }
-//
-//}
 
 
 }
--- app/src/main/java/com/example/tryonlysports/ui/facility/SportsFacilityViewModel.kt
+++ app/src/main/java/com/example/tryonlysports/ui/facility/SportsFacilityViewModel.kt
@@ -4,10 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+<<<<<<< Updated upstream
 class SportsFacilityViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
+=======
+/**
+ * This is the ViewModel that stores and manages UI related data in the lifecycle of the Sports Facility fragment.
+ *
+ * @author Li Rui, Ye Ziyuan, Wan Qian
+ */
+class SportsFacilityViewModel : ViewModel() {
+
+    /**
+     * The information as mutable live private data.
+     */
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+
+    /**
+     * The information as live data.
+     */
+>>>>>>> Stashed changes
     val text: LiveData<String> = _text
 }
